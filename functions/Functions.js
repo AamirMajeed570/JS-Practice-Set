1142d040998a43cb6860e06a488fa24cd5f434e7
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-02-29 11:47:11 +0530

    Functions

---
+++ functions/Functions.js
@@ -0,0 +1,18 @@
+// function sayHello(username) {
+//         return `Hey! How are You . ${username} `
+// }
+
+// sayHello("aamir");
+// console.log(sayHello("aamir"));
+
+// Objects
+const obj = {
+    name:"Aamir Majeed Khan",
+    role:"SDE"
+}
+
+function greet(myobj){
+    console.log(`The name of a user is ${myobj.name} with specified role of ${myobj.role}`)
+}
+
+greet(obj)
