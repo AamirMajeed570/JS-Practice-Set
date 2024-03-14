bc6126a86a45ebf49eeec156df836cde6c91d59d
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-03-14 15:40:09 +0530

    Array Map

--- 03Array/Array.js
+++ 03Array/Array.js
@@ -1,33 +1,40 @@
-let arr = [1,2,3,4,5,6];
-// console.log(arr);
+// let arr = [1,2,3,4,5,6];
+// // console.log(arr);
 
-const value = arr.reduce((accumalator,currentValue)=>{
-    return accumalator + currentValue;
-})
-// console.log(`The Sum of Elements in array are ${value}`);
+// const value = arr.reduce((accumalator,currentValue)=>{
+//     return accumalator + currentValue;
+// })
+// // console.log(`The Sum of Elements in array are ${value}`);
 
-const res = arr.filter((val)=>{
-    return val%2==0
+// const res = arr.filter((val)=>{
+//     return val%2==0
 
-})
+// })
 
-// console.log(res)
+// // console.log(res)
 
-let newArray = [];
-arr.forEach((value)=>{
-    if(value%2==0)
-    {
-        newArray.push(value)
-    }
+// let newArray = [];
+// arr.forEach((value)=>{
+//     if(value%2==0)
+//     {
+//         newArray.push(value)
+//     }
+// })
+// // console.log(newArray)
+// newArray.forEach((item)=>{
+//     // console.log(item);
+// })
+// // Method Chaining Technique
+// let square = arr.map((item)=>{
+//     return item * item
+// }).filter((val)=>{
+//     return val % 3==0
+// })
+// console.log(square)
+let arr = new Array();
+arr = [10,20,30,40,50];
+console.log(arr);
+let result = arr.map((element)=>{
+    return Math.floor(Math.sqrt(element));
 })
-// console.log(newArray)
-newArray.forEach((item)=>{
-    // console.log(item);
-})
-// Method Chaining Technique
-let square = arr.map((item)=>{
-    return item * item
-}).filter((val)=>{
-    return val % 3==0
-})
-console.log(square)
+console.log(result);
