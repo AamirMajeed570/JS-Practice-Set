719e7f93cfee9e906d70adf37d7ef698da8b716f
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-03-19 11:31:37 +0530

    Async

--- Async/index.js
+++ Async/index.js
@@ -1,19 +1,29 @@
-console.log(1); // 1 2 4 6 20 30 50
+// console.log(1); // 1 2 4 6 20 30 50
 
-setTimeout(() => {
-    console.log(20);
-}, 0);
+// setTimeout(() => {
+//     console.log(20);
+// }, 0);
 
-console.log(2);
+// console.log(2);
 
-setTimeout(()=>{
-    console.log(30)
-},2)
+// setTimeout(()=>{
+//     console.log(30)
+// },2)
 
-console.log(4);
+// console.log(4);
 
-setTimeout(()=>{
-    console.log(50);
-},3)
+// setTimeout(()=>{
+//     console.log(50);
+// },3)
 
-console.log(6);
+// console.log(6);
+
+const sayAfterTwoSecond = function(name){
+    console.log(`Name is ${name}`);
+}
+
+setTimeout(sayAfterTwoSecond,2000,"aamir");
+
+setTimeout((name)=>{
+    console.log(`Name is ${name}`);
+},2000,"aamir")
