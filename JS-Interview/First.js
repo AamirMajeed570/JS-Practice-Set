b217f989b6ddb5ab1687da0d5217cb7d0f33af8e
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-03-21 13:09:44 +0530

    Git Configured

---
+++ JS-Interview/First.js
@@ -0,0 +1,42 @@
+// const input1 = {
+//     a:1,
+//     b:2,
+//     c:3,
+//     d:10,
+//     e:12
+// }
+// const input2 = {
+//     a:2,
+//     e:12,
+//     f:6,
+//     d:10
+// }
+// let input3 = {}
+// function compareObject(input1,input2){
+//     // console.log(input1,input2)
+//     for(let i in input1){
+//         if(input1[i]==input2[i])
+//         {
+//             input3[i]=input1[i];
+//         }
+//     }
+// }
+
+// compareObject(input1,input2)
+// console.log(input3)
+
+
+// const inputArray = [1,2,-2,11,7,1];
+// inputArray.sort((a,b)=>a-b);
+// console.log(inputArray)
+// // for (let index = 0; index < array.length; index++) {
+    
+// // }
+// let len = inputArray.length-2;
+// console.log(`Second Largest Element is ${inputArray[len]}`)
+
+const input = [2,7, 11,4, -2];
+console.log(input)
+input.push(input.shift());
+input.push(input.shift());
+console.log(input);
