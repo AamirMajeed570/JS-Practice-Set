275e3abb272b84e2303b0851c84d608b01a889a7
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-06-04 19:09:59 +0530

    Second Largest Element in an Arrray

--- JS_Interview/Array.js
+++ JS_Interview/Array.js
@@ -24,6 +24,35 @@
 // console.log(totalSum);
 
 // Reduce method in JS
-let arr = [1,2,3,4,5];
-let res = arr.reduce((a,b)=>a+b,-10)
-console.log(res);
+// let arr = [1,2,3,4,5];
+// let res = arr.reduce((a,b)=>a+b,-10)
+// console.log(res);
+
+// let obj1 ={a:1,b:2,c:3,d:10,e:12};
+// let obj2 = {a:2,e:12,f:6,d:10}
+// let obj3 = {}
+// function compare(obj1,obj2){
+//     for(let i in obj1){
+//         if(obj1[i]==obj2[i]){
+//             obj3[i]=obj1[i];
+//         }
+//     }
+//     return obj3;
+// }
+// console.log(compare(obj1,obj2));
+
+// Second Largest Element in an Array
+
+let arr = [1,2,-2,11,7,1];
+let largest = -Infinity;
+let secLargest = -Infinity;
+for(let i=1;i<arr.length;i++){
+    if(arr[i]>largest){
+        secLargest = largest;
+        largest = arr[i]
+    }
+    else if(arr[i]> secLargest && arr[i]!== largest){
+        secLargest = arr[i];
+    }
+}
+console.log(secLargest);
