ed076145c7dfae7fe70886061dba83b28c1cab73
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-06-12 14:08:02 +0530

    string methods

---
+++ strings/index.js
@@ -0,0 +1,7 @@
+let str = "aamir majeed khan";
+// console.log(str.reverse())
+// let newStr = str.split("");
+// console.log(newStr);
+// console.log(newStr.reverse())
+
+// console.log(str.charCodeAt(7));
